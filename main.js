4dcc1d8b7a0148460deb2e72529dd3d622de8d2f
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-02-16 15:58:09 +0700

    Refactor comments and implement image toggle function
    Updated comments and added image_toggle function to toggle image visibility.

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-// this is a highly lazy person you just can't make a js file right
-//  |  |
-//  |  |
-//  \  /
-//   \/
-//  you
+// look who finally decided it's time for js
+// do ya even know when to type words on me
+// huh? f*cking son of a faggot's wanker's ballox's bitch
+function image_toggle() {
+  let img_tag = document.getElementbyId("image").innerHTML;
+  if (img_tag.includes("hidden")) {
+    document.getElementbyId("image") = "\n  <img src="cat.jpg" width="255" height="255" style="display: block; margin: auto;">\n";
+  }
+  else {
+    document.getElementbyId("image") = "\n  <img src="cat.jpg" width="255" height="255" style="display: block; margin: auto;" hidden>\n";
+  }
+}
